5f6448e099e177773c7359aa2658a3f47e6d9770
MatheusOliveiraT <15matheusdeoliveira@gmail.com>
2024-10-22 14:17:07 -0300

    Pratica2

--- Pratica1/pratica1.m
+++ Pratica1/pratica1.m
@@ -2,8 +2,10 @@ pkg load image;
 
 img = imread("tungsten_filament_shaded.tif");
 shade = imread("tungsten_sensor_shading.tif");
+imshow(img)
 img = im2double(img);
 shade = im2double(shade);
 img = img ./ shade;
 
+figure()
 imshow(img)
